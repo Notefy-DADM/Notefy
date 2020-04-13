4b8109853453644aab0945e9a6598718bc250f3d
Alejandro Gómez <algono@inf.upv.es>
2020-04-13 19:29:09 +0200

    CheckBoxBlock support

--- app/src/main/java/io/github/notefydadm/notefy/Model/CheckBoxBlock.java
+++ app/src/main/java/io/github/notefydadm/notefy/Model/CheckBoxBlock.java
@@ -1,7 +1,37 @@
 package io.github.notefydadm.notefy.Model;
 
+import androidx.annotation.Nullable;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckBoxBlock extends TextBlock {
+    private static final Pattern CheckBoxPattern = Pattern.compile("\\s*?- \\[([\\sx])] (.*)");
+
+    public static boolean matches(CharSequence input) {
+        return CheckBoxPattern.matcher(input).matches();
+    }
+
+    @Nullable
+    public static CheckBoxBlock fromLine(CharSequence line) {
+        final Matcher checkBoxMatcher = CheckBoxPattern.matcher(line);
+        if (checkBoxMatcher.matches()) {
+            String checkMark = checkBoxMatcher.group(1);
+            String text = checkBoxMatcher.group(2);
+            // The regex ensures that there is only either a whitespace or an 'x'
+            boolean isChecked = !(checkMark == null || checkMark.trim().isEmpty());
+            return new CheckBoxBlock(text, isChecked);
+        } else {
+            return null;
+        }
+    }
+
     private boolean isChecked;
+
+    public CheckBoxBlock(String text, boolean isChecked) {
+        super(text);
+        this.isChecked = isChecked;
+    }
 
     public CheckBoxBlock(String text, String fontFamily, String fontSize, boolean isChecked) {
         super(text, fontFamily, fontSize);
