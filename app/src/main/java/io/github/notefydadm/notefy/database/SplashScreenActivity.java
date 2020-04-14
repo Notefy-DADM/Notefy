2d598da9406e525341c865a6f9eeb8adfcb993a2
andresen1998 <andresen1998@hotmail.com>
2020-04-14 18:35:25 +0200

    Created Splash screen

---
+++ app/src/main/java/io/github/notefydadm/notefy/database/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package io.github.notefydadm.notefy.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.notefydadm.notefy.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
