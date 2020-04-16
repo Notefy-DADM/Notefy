d0f7c3c280f24b5b988409caab221bc4f2d3625d
andresen1998 <andresen1998@hotmail.com>
2020-04-16 10:54:48 +0200

    Splash screen to main activity

--- app/src/main/java/io/github/notefydadm/notefy/database/SplashScreenActivity.java
+++ app/src/main/java/io/github/notefydadm/notefy/database/SplashScreenActivity.java
@@ -2,9 +2,14 @@ package io.github.notefydadm.notefy.database;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import io.github.notefydadm.notefy.R;
+import io.github.notefydadm.notefy.view.activities.MainActivity;
+import io.github.notefydadm.notefy.view.autentication.AuthenticationActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,9 +17,27 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        initAll();
     }
 
     private void initAll(){
-        SingletonDatabase.getInstance().init();
+
+
+        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+            openLogin();
+        }else{
+            openMain();
+            SingletonDatabase.getInstance().init();
+        }
+    }
+
+    private void openLogin(){
+        Intent intent = new Intent(this, AuthenticationActivity.class);
+        startActivity(intent);
+    }
+
+    private void openMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
