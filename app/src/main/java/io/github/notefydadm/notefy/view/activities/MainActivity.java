607375f01d52a6cf63ef9549a250de022c6dc148
andresen1998 <andresen1998@hotmail.com>
2020-04-24 19:40:26 +0200

    Corrected landscape

--- app/src/main/java/io/github/notefydadm/notefy/view/activities/MainActivity.java
+++ app/src/main/java/io/github/notefydadm/notefy/view/activities/MainActivity.java
@@ -61,6 +61,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (savedInstanceState != null) {
             //return;
         }
+        noteViewModel.getSelectedNote().observe(this, new Observer<Note>() {
+            @Override
+            public void onChanged(Note note) {
+                if(note!=null){
+                    if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragNoteText, noteFragment).addToBackStack(null).commit();
+
+                    } else {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentPortraitContainer, noteFragment).addToBackStack(null).commit();
+
+                    }
+                }
+            }
+        });
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             //  landscape
